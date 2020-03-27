90bbf29604ad82b11c4e12ccfbceedb39cfadcc6
Eric Settels <settels.eric@gmail.com>
2020-03-27 09:20:39 -0700

    add event listerners to 'next' buttons to cycle to next question

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  //to do
+  for (let i = 2; i <= 5; i++) {
+    $(`#toQ${i}`).click(function(event) {
+      event.preventDefault()
+      $(`#q${i - 1}`).addClass('no-show')
+      $(`#q${i}`).removeClass('no-show')
+    })
+  }
 })
