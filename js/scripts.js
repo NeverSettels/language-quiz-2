eaa7da72d61f8aae11e47e3aeb59c8542b5da9e5
Eric Settels <settels.eric@gmail.com>
2020-03-27 10:11:21 -0700

    add logic to sort scores for languages and display results

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,35 @@ $(document).ready(function() {
 
   $('#quiz-form').submit(function(event) {
     event.preventDefault()
-    let cssScore = 0
-    let scssScore = 0
-    let jsScore = 0
-    let csScore = 0
-    let pythoScore = 0
+    let css = { name: 'CSS', score: 0 }
+    let scss = { name: 'SCSS', score: 0 }
+    let js = { name: 'JavaScript', score: 0 }
+    let cs = { name: 'C#', score: 0 }
+    let python = { name: 'Python', score: 0 }
+    let binary = { name: 'Binary', score: 0 }
+    let langArr = [css, scss, js, cs, python, binary]
+    let message = 'Based on your answers this is your list from Most recomended to Least recommended'
+
     const generation = $("input[name='generation']:checked").val()
     const design = $("input[name='design']:checked").val()
     const logic = $("input[name='logic']:checked").val()
     const games = $("input[name='games']:checked").val()
     const programming = $("input[name='programming']:checked").val()
+
+    if (programming === 'no') {
+      message = "If you dont really like proggraming then the order won't matter!"
+    } else {
+      console.log('you shouldnt be here')
+    }
+
+    langArr.sort((a, b) => b.score - a.score)
+    $('#message').text(message)
+    langArr.forEach(language => {
+      $('#results-list').append(`<ol>${language.name}</ol>`)
+    })
+
+    $('#q5').addClass('no-show')
+    $('#quiz-form').addClass('no-show')
+    $('#results').removeClass('no-show')
   })
 })
